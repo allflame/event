2e65f21a769ba8098fd6f7a816280f7966c8611d
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-05-10 15:06:20 +0300

    Added EventExtension

---
+++ src/Extension/EventExtension.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Event
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types=1);
+
+namespace Vainyl\Event\Extension;
+
+use Vainyl\Core\Extension\AbstractExtension;
+
+/**
+ * Class EventExtension
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class EventExtension extends AbstractExtension
+{
+}
